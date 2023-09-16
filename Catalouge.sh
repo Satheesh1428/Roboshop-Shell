445edac2cd7d4af537298afb7c3894a0cedf6ddd
ASatheesh <satheeshovsn@gmail.com>
2023-09-16 15:44:21 +0530

    Frontend project DNS records add

---
+++ Catalouge.sh
@@ -0,0 +1,18 @@
+cp Catalouge.service /etc/systemd/system/catalogue.service
+cp Mongo.repo /etc/yum.repos.d/mongo.repo
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+yum install mongodb-org-shell -y
+mongo --host mongodb.devopsovsn.online </app/schema/catalogue.js
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+dnf install mongodb-org-shell -y
+
---
+++ MySQL.sh
@@ -0,0 +1,7 @@
+cp /etc/yum.repos.d/mysql.repo
+yum module disable mysql -y
+yum install mysql-community-server -y
+mysql_secure_installation --set-root-pass RoboShop@1
+mysql -uroot -pRoboShop@1
+systemctl enable mysqld
+systemctl start mysqld
