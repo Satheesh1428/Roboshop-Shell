ad54dbad807f1dcb0d297cb767762dd325f98217
ASatheesh <satheeshovsn@gmail.com>
2023-09-28 07:24:57 +0530

    Dispatchcmt1

--- MySQL.sh
+++
@@ -1,7 +0,0 @@
-cp Mysql.repo /etc/yum.repos.d/Mysql.repo
-yum module disable mysql -y
-yum install mysql-community-server -y
-mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
-systemctl enable mysqld
-systemctl restart mysqld
