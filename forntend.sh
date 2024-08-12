8882cf34a73bf541b004a81dd70b4d860fe5611a
ASatheesh <satheeshovsn@gmail.com>
2024-08-13 00:40:21 +0530

    Push code from remote github to aws server test2

--- forntend.sh
+++ forntend.sh
@@ -6,7 +6,7 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
-unzip /temp/frontend.zip
+unzip /tmp/frontend.zip
 
 systemctl enable nginx
 systemctl restart nginx
