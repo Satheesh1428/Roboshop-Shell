bb8e114c64862359d1c8d1e55bb9791aa5b0f271
ASatheesh <satheeshovsn@gmail.com>
2024-08-13 00:38:10 +0530

    Push code from remote github to aws server test2

--- forntend.sh
+++ forntend.sh
@@ -3,9 +3,9 @@ dnf install nginx -y
 cp roboshop.conf  /etc/nginx/default.d/roboshop.conf
 
 rm -rf /usr/share/nginx/html/*
-curl -o /temp/forntend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /temp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
-unzip /temp/forntend.zip
+unzip /temp/frontend.zip
 
 systemctl enable nginx
 systemctl restart nginx
